3569a5b13673c3e10523bf1b81dd59373c554a51
Mef1stofe1 <kirkovau@gmail.com>
2018-07-11 12:12:06 +0300

    Initial Commit

---
+++ src/app/manager.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+import {environment} from './models/environment'
+
+const apiUrl = environment.apiUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ManagerService {
+
+ 
+  constructor() { }
+
+connectToServer(){
+const connectionString = 'http://api.football-data.org/v2/';
+
+}
+
+requestdDataFromServer(){
+
+}
+
+
+
+}
---
+++ src/app/models/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    apiUrl: 'http://api.football-data.org/v2/'
+};
