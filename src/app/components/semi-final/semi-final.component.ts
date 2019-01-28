26d6e5af3f6b6b7cc7627637a3d6e82b3f50755b
Mef1stofe1 <kirkovau@gmail.com>
2019-01-28 20:14:06 +0200

    PlayOff feature was added

---
+++ src/app/components/semi-final/semi-final.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-semi-final',
+  templateUrl: './semi-final.component.html',
+  styleUrls: ['./semi-final.component.css']
+})
+export class SemiFinalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
