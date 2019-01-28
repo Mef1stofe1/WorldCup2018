9c996044cd8720b7c8d945886f42078678939622
Mef1stofe1 <kirkovau@gmail.com>
2019-01-28 20:11:24 +0200

    Routing modifications

--- src/app/app-router/app-router.module.ts
+++ src/app/app-router/app-router.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GroupTableComponent } from '../components/group-component/group-table.component';
 import { PageNotFoundComponent } from '../components/PageNotFound/page-not-found.component';
+import { QuarterFinalComponent } from '../components/quarter-final/quarter-final.component';
+import { SemiFinalComponent } from '../components/semi-final/semi-final.component';
+import { PlayOffComponent } from '../components/play-off/play-off.component';
 
  const routes: Routes = [
 
   { path: 'groups', component: GroupTableComponent },
+  { path: 'play-off', component: PlayOffComponent },
+  { path: 'quarter-final', component: QuarterFinalComponent },
+  { path: 'semi-final', component: SemiFinalComponent },
 
   { path: '', redirectTo: 'groups', pathMatch:'full' },
   { path: '**', component: PageNotFoundComponent }
@@ -13,6 +19,6 @@ import { PageNotFoundComponent } from '../components/PageNotFound/page-not-found
 
 @NgModule({
   exports: [RouterModule],
-  imports: [RouterModule.forRoot(routes, { enableTracing: true })]
+  imports: [RouterModule.forRoot(routes)]
 })
 export class AppRouterModule { }
